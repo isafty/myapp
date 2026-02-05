cfffa74cc8c055d75e7adef70266b17f303a42ff
ealsafty <ealsafty@gmail.com>
2026-02-05 08:33:27 +0000

    start

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/core/utils/app_router.dart';
+import 'package:myapp/core/utils/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      theme: AppTheme.lightTheme,
+      routerConfig: router,
+    );
+  }
+}
