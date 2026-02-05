6061b78f13a9994aa9cb60c9a110ceff7a552895
ealsafty <ealsafty@gmail.com>
2026-02-05 09:21:14 +0000

    add fonts&text style and splash view

---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/core/utils/app_colors.dart';
+
+class AppTextStyle {
+  AppTextStyle._();
+  static const pacifico400style64 = TextStyle(
+    fontSize: 64,
+    fontWeight: FontWeight.w400,
+    color: AppColors.deepBrown,
+    fontFamily: "Pacifico",
+  );
+}
--- lib/features/splash/ui/view/splash_view.dart
+++ lib/features/splash/ui/view/splash_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/core/utils/app_strings.dart';
+import 'package:myapp/core/utils/app_text_style.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Splash Screen')));
+    return const Scaffold(
+      body: Center(
+        child: Text(AppStrings.appName, style: AppTextStyle.pacifico400style64),
+      ),
+    );
   }
 }
