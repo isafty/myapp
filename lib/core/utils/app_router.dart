c203f13ff3ec65dec36dbe4bf6172fffcdb350e6
ealsafty <ealsafty@gmail.com>
2026-02-05 09:31:21 +0000

    navigate auto to onboarding

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,9 +1,13 @@
 import 'package:go_router/go_router.dart';
+import 'package:myapp/features/on_boarding/ui/view/on_boarding_view.dart';
 import 'package:myapp/features/splash/ui/view/splash_view.dart';
 
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashView()),
-    
-    ],
+    GoRoute(
+      path: '/onBoarding',
+      builder: (context, state) => const OnBoardingView(),
+    ),
+  ],
 );
--- lib/features/splash/ui/view/splash_view.dart
+++ lib/features/splash/ui/view/splash_view.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:myapp/core/utils/app_strings.dart';
 import 'package:myapp/core/utils/app_text_style.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      GoRouter.of(context).push('/onBoarding');
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
